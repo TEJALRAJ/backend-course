52021c241c321965b12ad855055fe6e5349f2109
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2022-09-23 21:06:10 +0530

    CRUD in mongoose

---
+++ 07-Mongoose/index.js
@@ -0,0 +1,147 @@
+
+import mongoose from "mongoose"
+import {faker} from '@faker-js/faker'
+import crypto from 'crypto'
+
+// connect to the database - local db or remote db
+// connection string - mongodb://127.0.0.1:27017/test-db
+// mongodb+srv://pt-web-04-b:<password>@cluster0.aajb25r.mongodb.net/?retryWrites=true&w=majority
+
+async function connectDatabbase() {
+    const connection = 'mongodb://127.0.0.1:27017/pt-web-05-test'
+
+    return new Promise((resolve, reject) => {
+        mongoose.connect(connection)
+        .then(() => {
+            console.log('Connected to database');
+            resolve();
+        })
+        .catch((err) => {
+            console.log('Could not connect to database')
+            reject(err);
+        })
+    })
+}
+
+// For each collection
+
+// First we need to define a schema - blueprint of our data
+
+// plain way to define the schema
+const userSchema = new mongoose.Schema({ // Class
+    name: String,
+    email: String,
+    age: Number,
+    image: String,
+    verifiedEmail: Boolean,
+    balance: Number,
+})
+
+
+// We need to create a model from that schema
+const userModel = mongoose.model('User', userSchema, 'users') // 'User' -> 'users'
+
+// Model will provide us all the function to do CRUD operations on that collection
+
+async function test() {
+
+    await connectDatabbase();
+
+    // we will execute some queries on collections from here
+
+
+
+    // Create User
+
+    // let user = await userModel.create({
+    //     name: 'John Doe',
+    //     email: 'john.doe@john.doe',
+    //     age: 32,
+    //     image: 'https://example.com/image.jpeg',
+    //     verifiedEmail: false,
+    //     balance: 23000.34
+    // })
+
+
+
+
+    // Create Multiple - Insert Many
+
+    // const users = []
+
+    // for (let i = 0; i < 100; i++) {
+    //     let user = {
+    //         name: faker.name.fullName(),
+    //         email: faker.internet.email(),
+    //         age: crypto.randomInt(16, 55),
+    //         image: faker.image.avatar(),
+    //         verifiedEmail: Boolean(crypto.randomInt(0, 2)),
+    //         balance: crypto.randomInt(0, 55000),
+    //     }
+
+    //     users.push(user);
+    // }
+
+    // await userModel.insertMany(users);
+
+    // console.log('Users added')
+
+
+
+
+    // Find All
+    // let users = await userModel.find()
+
+    // console.log(users); // empty array
+
+
+
+
+    // Find One
+
+    // let user = await userModel.findOne({
+    //     email: 'Triston60@gmail.com'
+    // })
+
+
+
+
+
+    // Update One
+
+    // await userModel.findOneAndUpdate({
+    //     email: 'Triston60@gmail.com'
+    // }, {
+    //     email: 'Triston601@gmail.com'
+    // })
+
+    // user.email = 'jason.doe@john.doe'
+    // user.name = 'Jason Doe'
+
+
+    // Update Many
+
+    // await userModel.updateMany({
+    //     age: 99
+    // }, {
+    //     age: 32
+    // })
+
+
+
+    // Delete One
+    // await userModel.findOneAndDelete({
+    //     email: 'Jayne.Jacobi@yahoo.com'
+    // })
+
+    // Delete Many
+    // await userModel.deleteMany({
+    //     age: {
+    //         $mod: [2, 0]
+    //     }
+    // })
+
+
+}
+
+test();
